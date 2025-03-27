ee07dad97fe4cac60339dbecf38f4c8cbbaf4a9e
Loicsoh <www.sohloic132@gmail.com>
2025-03-27 16:28:18 +0100

    nhy

--- cour-php/TP-02/src/connexion.php
+++ cour-php/TP-02/src/connexion.php
@@ -1,13 +1,51 @@
 <?php
   require_once'database.php';
-  function handlerpostRequest($pdo);
-
+    function handlerpostRequest($pdo){
+ 
   // verification du type de request
-  if($_SERVER['REQUEST_METHOD'] == 'POST'){
-  return Register($pdo);  
+  if($_SERVER['REQUEST_METHOD'] !== 'POST'){
+  return "ok";  
   }
 
   // reception des donnees du formulaire
+  $mailconnect = htmlspecialchars($_POST['mailconnect']);
+  $mdpconnect = $_POST['mdpconnect'];
+  
+  if(empty($mailconnect) || empty($mdpconnect)){
+   return "les champs doivent etre remplire";  
+  }
+
+  return authentificateUser($pdo, $mailconnect, $mdpconnect);
+} 
+
+  function authentificateUser($pdo, $mailconnect, $mdpconnect){
+  
+  // verification du mail
+
+  $sql = "SELECT * FROM membre WHERE mail = :mailconnect";
+  $reqmail = $pdo->prepare($sql);
+  $reqmail->execute(compact('mailconnect'));
+  $mailExist = $reqmail->rowCount();
+
+  if (!$mailExist) {
+    return "L'Email n'existe pas";
+  }
+
+  $userinfo =$reqmail->fetch();
+  var_dump($userinfo);
+
+  echo "<pre>";
+  print_r($userinfo ['mdp']);
+  echo "</pre>";
+
+  if (!password_verify($mdpconnect,$userinfo['mdp'])){
+    return 'Mauvais mot de pass !';
+  }
+  return 'succes!';
+}
+
+  $erreur = handlerpostRequest($pdo);
+
 ?>
 
 
@@ -24,8 +62,8 @@
     <h2>Connexion</h2>
     <br /><br />
     <form method="POST" action="">
-      E-mail : <input type="email" name="mailconnect" placeholder="Mail" /> <br><br>
-      PassWord : <input type="password" name="mdpconnect" placeholder="Mot de passe" />
+      E-mail : <input type="email" name="mailconnect" value="<?=$mailconnect??''?>" placeholder="Mail" /> <br><br>
+      PassWord : <input type="password" name="mdpconnect" value="<?=$mdpconnect??''?>" placeholder="Mot de passe" />
       <br /><br />
       <input type="submit" value="Se connecter !" />
     </form>
