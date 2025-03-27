4b58ca2e747b286db80c67014610fd69a2d145db
Loicsoh <www.sohloic132@gmail.com>
2025-03-27 13:52:24 +0100

    mjh

---
+++ cour-php/TP-02/connexion.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>TUTO PHP</title>
+  <meta charset="utf-8">
+</head>
+
+<body>
+  <div align="center">
+    <h2>Connexion</h2>
+    <br /><br />
+    <form method="POST" action="">
+      E-mail : <input type="email" name="mailconnect" placeholder="Mail" /> <br><br>
+      PassWord : <input type="password" name="mdpconnect" placeholder="Mot de passe" />
+      <br /><br />
+      <input type="submit" value="Se connecter !" />
+    </form>
+    <?php
+         if (isset($erreur)) {
+            echo '<font color="red">' . $erreur . "</font>";
+         }
+         ?>
+  </div>
+</body>
+
+</html>
